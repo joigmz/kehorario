a3c90a567fdf3d5d5a77e6549f3cb55047931fab
joigmz <jose.izam99@gmail.com>
2022-02-10 22:47:53 -0300

    side bar

--- static/javascript/javascript.js
+++ static/javascript/javascript.js
@@ -1,3 +1,14 @@
 $(function(){
     $( 'input[type="checkbox"]' ).prop('checked', false);
 });
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("wrapper").style.width = "100vw";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("wrapper").style.width = "0vw";
+    document.body.style.backgroundColor = "white";
+}
